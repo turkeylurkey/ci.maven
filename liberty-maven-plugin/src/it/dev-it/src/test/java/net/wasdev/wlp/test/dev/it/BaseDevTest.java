fbb2d4a32f6c272576862a9e5ac6ffe1f7fe031d
Turkeylurkey <turkeyonmarblerye@gmail.com>
2021-01-11 16:53:13 -0500

    Fix syntax

--- liberty-maven-plugin/src/it/dev-it/src/test/java/net/wasdev/wlp/test/dev/it/BaseDevTest.java
+++ liberty-maven-plugin/src/it/dev-it/src/test/java/net/wasdev/wlp/test/dev/it/BaseDevTest.java
@@ -121,7 +121,7 @@ public class BaseDevTest {
       String e = runCmd("id");
       e += runCmd("pwd");
       e += runCmd("ls -la");
-      e += runCmd("ls -l" + logFile.getName())
+      e += runCmd("ls -l" + logFile.getName());
       assertTrue(e, verifyLogMessageExists("CWWKF0011I", 220000));
       if (isDevMode) {
          assertTrue(verifyLogMessageExists("Liberty is running in dev mode.", 60000));
