033f2e5fc781c73667e187204668e6413dfa3741
Paul Gooderham <turkeyonmarblerye@gmail.com>
2026-06-25 18:04:34 -0400

    Only run dev-it
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

--- liberty-maven-plugin/src/it/dev-it/src/test/java/net/wasdev/wlp/test/dev/it/MultiModuleHotTestingTest.java
+++ liberty-maven-plugin/src/it/dev-it/src/test/java/net/wasdev/wlp/test/dev/it/MultiModuleHotTestingTest.java
@@ -60,7 +60,8 @@ public class MultiModuleHotTestingTest extends BaseMultiModuleTest {
         assertTrue(targetEarClass.lastModified() > targetLastModified);
 
         // check tests running message for jar since we expect jar unit tests to fail (after modifying)
-        assertTrue(verifyLogMessageExists("Running unit tests for guide-maven-multimodules-jar ..." + getLogTail(), 3000));
+        assertTrue("Searchiung for Running unit tests msg:" + getLogTail(), 
+            verifyLogMessageExists("Running unit tests for guide-maven-multimodules-jar ...", 3000));
         
         verifyTestsRan("guide-maven-multimodules-war", "guide-maven-multimodules-ear");
 
